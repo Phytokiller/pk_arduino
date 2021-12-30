1d21068c03570f736ff5921f7c0bb5ffd01d307c
acetayls <pierrick.saillant@yahoo.fr>
2021-12-30 10:17:33 +0100

    timeout on alarm 1&2

--- _pk_ardbox/alarm.ino
+++ _pk_ardbox/alarm.ino
@@ -39,5 +39,6 @@ void process_Alarm_High(int data) { // called by com
 }
 
 void set_timeout_alarm(bool state) {
-  digitalWrite(ALARM1_PIN, !state);
+  //digitalWrite(ALARM1_PIN, !state);
+  digitalWrite(ALARM2_PIN, state);
 }
